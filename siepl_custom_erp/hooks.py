cbdecb34f50c60ec230f271921e47670333c2080
Suhas Reddy <suhasreddy2003.sr@gmail.com>
2026-05-21 11:29:05 +0000

    [First commit]- All initial changes moved to custom app

--- siepl_custom_erp/hooks.py
+++ siepl_custom_erp/hooks.py
@@ -23,6 +23,11 @@ app_license = "mit"
 
 # Includes in <head>
 # ------------------
+fixtures = [
+    {
+        "dt": "Client Script"
+    }
+]
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/siepl_custom_erp/css/siepl_custom_erp.css"
